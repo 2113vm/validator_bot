4e3e795c10583de83e3609e665c164bd5ed96fc3
v.markov <21138532110q@gmail.com>
2020-05-21 23:24:00 +0500

    completed invoke task

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from invoke import task
 
 from bot import Bot
-from metrics import Validator
+from metrics import OCRValidator, TextDetectionValidator
 
 
 @task
@@ -9,4 +9,7 @@ def run_telegram_bot(cfg):
     Bot(token=cfg.telegram_bot.token,
         proxy_url=cfg.telegram_bot.proxy_url,
         proxy_kwargs=cfg.telegram_bot.proxy_kwargs,
-        validator=Validator()).run()
+        cig_ocr_validator=OCRValidator(cfg.ocr.cig.gt_path),
+        pricetag_ocr_validator=OCRValidator(cfg.ocr.pricetag.gt_path),
+        cig_text_detection_validator=TextDetectionValidator(cfg.text_detection.cig.gt_path),
+        pricetag_text_detection_validator=TextDetectionValidator(cfg.text_detection.pricetag.gt_path)).run()
